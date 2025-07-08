e4adedbd1fd674e66f46046787737d8aa3e10b7a
vivek212006 <vivek.h@s.amity.edu>
2025-07-08 22:15:46 +0530

    elsif

---
+++ ex4.py
@@ -0,0 +1,8 @@
+elif m == "o":
+    z = int(input("Enter num 1: "))
+    j = int(input("Enter num 2: "))
+    if j != 0:
+        y = z / j
+        print(y, "is the division")
+    else:
+        print("Error: Division by zero")
