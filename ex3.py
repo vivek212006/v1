0dd85f2e019c0e5ab47a61112e97be1fcad37ffa
vivek212006 <vivek.h@s.amity.edu>
2025-07-08 22:11:50 +0530

    elif

---
+++ ex3.py
@@ -0,0 +1,10 @@
+elif m == "b":
+    z = int(input("Enter num 1: "))
+    j = int(input("Enter num 2: "))
+    y = z - j
+    print(y, "is the difference")
+elif m == "c":
+    z = int(input("Enter num 1: "))
+    j = int(input("Enter num 2: "))
+    y = z * j
+    print(y, "is the product")
